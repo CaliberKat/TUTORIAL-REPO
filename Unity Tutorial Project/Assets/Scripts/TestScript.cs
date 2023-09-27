909f81e92a061ccebf1e9969060dffcd953be977
CaliberKat <1636442@fife.ac.uk>
2023-09-27 12:11:17 +0100

    Second String

---
+++ Unity Tutorial Project/Assets/Scripts/TestScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string firstString;
+        string secondString;
+
+        firstString = "You Shall not PASS!!!";
+
+        secondString = "Fly, you fools..."
+
+        Debug.Log(firstString);
+        Debug.Log(secondString);
+    }
+}
